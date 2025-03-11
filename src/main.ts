ec2425d03451a44021e417e5b7db0529dda8dc85
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-03-11 22:08:11 +0700

    feat: initial feature

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
-const main = (): string => 'Hello, World!'
+import dts from 'rollup-plugin-dts'
+
+import { PLUGIN_NAME, PACKAGE_NAME } from './consts'
+
+import { findPlugin } from './utils'
+
+/**
+ * A
+ * [rollup-plugin-dts](https://www.npmjs.com/package/rollup-plugin-dts)
+ * with dual exports, allowing consumers to `import` or `require` it without
+ * needing to access the `.default` export in both **CommonJS** and
+ * **ES Module** formats.
+ *
+ * @see https://github.com/mnrendra/rollup-plugin-dts#readme
+ */
+const main = findPlugin<typeof dts>(dts, PLUGIN_NAME, PACKAGE_NAME)
 
 export default main
